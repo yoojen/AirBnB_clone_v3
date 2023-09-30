1419a8aeaf03b39b0daa811f0520a30be22174c5
eugene <eugeneemma7@gmail.com>
2023-09-30 04:12:28 -0700

    api files added

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+from api.v1.views import app_views
+from flask import jsonify
+@app_views.route('/status', strict_slashes=False)
+
+def showStatus():
+    """
+    showthe status of api
+    """
+    obj = {"status": "OK"}
+    return jsonify(obj)
