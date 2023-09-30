e68593939c4e98261e97b1261ab52cdbfb36f153
eugene <eugeneemma7@gmail.com>
2023-09-30 04:36:30 -0700

    added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@ from api.v1.views import app_views
 from models import storage
 from flask import jsonify
 
+
 @app_views.route('/status', strict_slashes=False)
 def showStatus():
     """
@@ -9,6 +10,7 @@ def showStatus():
     """
     obj = {"status": "OK"}
     return jsonify(obj)
+
 
 @app_views.route('/stats',  methods=['GET'], strict_slashes=False)
 def showStats():
@@ -19,7 +21,7 @@ def showStats():
             "amenities": storage.count('Amenityi'),
             "cities": storage.count('City'),
             "places": storage.count('Place'),
-            "reviews":storage. count('Review'),
+            "reviews": storage.count('Review'),
             "states": storage.count('State'),
             "users": storage.count('User')
             }
