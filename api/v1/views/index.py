9f65e257be0053fa0b0f04a950579cdfb6424f5f
eugene <eugeneemma7@gmail.com>
2023-09-30 04:32:33 -0700

    added stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,26 @@
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
-@app_views.route('/status', strict_slashes=False)
 
+@app_views.route('/status', strict_slashes=False)
 def showStatus():
     """
     showthe status of api
     """
     obj = {"status": "OK"}
+    return jsonify(obj)
+
+@app_views.route('/stats', strict_slashes=False)
+def showStats():
+    """
+    show stats of objects
+    """
+    obj = {
+            "amenities": storage.count('Amenityi'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews":storage. count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')
+            }
     return jsonify(obj)
